a1a026079020e200d673a6a8747f518baa9a68b7
Terence Mah <terence.mcj@gmail.com>
2020-03-17 23:31:29 +0800

    Fix some Checkstyle issues

--- src/main/java/seedu/address/logic/commands/ViewCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -3,12 +3,17 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import java.util.List;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 
+/**
+ * Shows the user the places visited, activities done and time spent
+ * with a chosen person from the Address Book.
+ */
 public class ViewCommand extends Command {
 
     public static final String COMMAND_WORD = "view";
@@ -59,12 +64,14 @@ public class ViewCommand extends Command {
         case KEYWORD_PLACE:
             model.showPlaceList(personToView);
             message = MESSAGE_PLACE + personToView.getName() + ".";
-            vt = ViewType.EVENTS; break;
+            vt = ViewType.EVENTS;
+            break;
 
         case KEYWORD_ACTIVITY:
             model.showActivityList(personToView);
             message = MESSAGE_ACTIVITY + personToView.getName() + ".";
-            vt = ViewType.EVENTS; break;
+            vt = ViewType.EVENTS;
+            break;
 
         default:
             message = MESSAGE_TIME + personToView.getName() + ".\nTotal individual time spent: "
