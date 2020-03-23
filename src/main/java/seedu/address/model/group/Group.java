29dce6f22a4a2b6ae57dc59466c906d21390df19
Raivat Shah <hiraivat@gmail.com>
2020-03-23 17:26:54 +0800

    modify Group Class structure

--- src/main/java/seedu/address/model/group/Group.java
+++ src/main/java/seedu/address/model/group/Group.java
@@ -1,10 +1,10 @@
 package seedu.address.model.group;
 
-import seedu.address.model.person.Name;
-
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Objects;
+
+import seedu.address.model.person.Name;
+import seedu.address.model.util.Time;
 
 /**
  * Represents the Social Group that a person is part of and spends time with.
@@ -18,13 +18,14 @@ public class Group {
 
     private Name name;
     private int groupId;
-    private ArrayList<Integer> members;
-    private float totalTime;
+    private Time timeSpent;
+    private ArrayList<Integer> memberIDs;
+    private ArrayList<Integer> eventIDs;
 
     public Group(Name name) {
         this.name = name;
-        this.members = new ArrayList<>();
-        this.totalTime = 0;
+        this.memberIDs = new ArrayList<>();
+        this.timeSpent = new Time(0, 0);
         this.groupId = groups;
         groups += 1;
     }
@@ -37,28 +38,24 @@ public class Group {
         return this.groupId;
     }
 
+    public Time getTimeSpent() {
+        return this.timeSpent;
+    }
+
     public void addPerson(int id) {
-        this.members.add(id);
+        this.memberIDs.add(id);
     }
 
     public void deletePerson(int id) {
-        this.members.remove(Integer.valueOf(id));
+        this.memberIDs.remove(Integer.valueOf(id));
     }
 
-    public void addTime(float additionalTime) {
-        this.totalTime += additionalTime;
-    }
-
-    public void subtractTime(float subtractTime) {
-        this.totalTime -= subtractTime;
-    }
-
-    public float getTotalTime() {
-        return this.totalTime;
+    public void setTimeSpent(Time time) {
+        this.timeSpent = time;
     }
 
     public ArrayList<Integer> getMembers() {
-        return this.members;
+        return this.memberIDs;
     }
 
     /**
@@ -68,19 +65,15 @@ public class Group {
      */
     public String printMemberList() {
         String build = "";
-        for (int i = 0; i < members.size(); i++) {
-            build += members.get(i) + " ";
+        for (int i = 0; i < memberIDs.size(); i++) {
+            build += memberIDs.get(i) + " ";
         }
         return build;
     }
 
-    public int getGroups() {
-        return groups;
-    }
-
     @Override
     public int hashCode() {
-        return Objects.hash(this.name, this.members);
+        return Objects.hash(this.name, this.memberIDs);
     }
 
     @Override
@@ -95,12 +88,12 @@ public class Group {
 
         Group g = (Group) o;
 
-        return (new HashSet<Integer>(this.getMembers())).equals(new HashSet<Integer>(g.getMembers()));
+        return (this.getName().equals(g.getName()) && this.getGroupId() == g.getGroupId());
     }
 
     @Override
     public String toString() {
-        if (this.members.isEmpty()) {
+        if (this.memberIDs.isEmpty()) {
             return "group ID: " + this.groupId + ". Name: " + this.name;
         } else {
             return "group ID: "
