64340768d662e465e8bbd46993a0c0abb161b39a
harrychengly <harrychengly@gmail.com>
2020-03-31 23:38:50 +0800

    added testcases

---
+++ src/test/java/seedu/address/logic/parser/SuggestCommandParserTest.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.ImportCommand;
+import seedu.address.logic.commands.SuggestCommand;
+
+public class SuggestCommandParserTest {
+    private SuggestCommandParser parser = new SuggestCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(
+                parser, "some invalid string",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SuggestCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsSuggestCommand() {
+        // no leading and trailing whitespaces
+        SuggestCommand expectedSuggestCommand =
+                new SuggestCommand("person");
+        assertParseSuccess(parser, " person", expectedSuggestCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " person", expectedSuggestCommand);
+    }
+}
