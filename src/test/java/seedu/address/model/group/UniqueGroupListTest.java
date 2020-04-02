fea867083f2acbc99a602e69de728d4753f2404c
Raivat Shah <hiraivat@gmail.com>
2020-04-02 18:04:43 +0800

    add testcases for UniqueGroupList

--- src/test/java/seedu/address/model/group/UniqueGroupListTest.java
+++ src/test/java/seedu/address/model/group/UniqueGroupListTest.java
@@ -1,5 +1,6 @@
 package seedu.address.model.group;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
@@ -45,6 +46,37 @@ public class UniqueGroupListTest {
         groups.add(group);
         uniqueGroupList.addAll(groups);
         assertThrows(DuplicateGroupException.class, () -> uniqueGroupList.addAll(groups));
+    }
+
+    @Test
+    public void setGroup_nullTargetGroup_throwsNullPointerException() {
+        Group group = new Group(new Name("Soc Friends"));
+        assertThrows(NullPointerException.class, () -> uniqueGroupList.setGroup(null, group));
+    }
+
+    @Test
+    public void setGroup_nullEditedGroup_throwsNullPointerException() {
+        Group group = new Group(new Name("Soc Friends"));
+        assertThrows(NullPointerException.class, () -> uniqueGroupList.setGroup(group, null));
+    }
+
+    @Test
+    public void setGroup_editedGroupHasNonUniqueIdentity_throwsDuplicatePersonException() {
+        Group group = new Group(new Name("Soc Friends"));
+        uniqueGroupList.addGroup(group);
+        Group group1 = new Group(new Name("Cinnamon Friends"));
+        uniqueGroupList.addGroup(group1);
+        assertThrows(DuplicateGroupException.class, () -> uniqueGroupList.setGroup(group, group1));
+    }
+
+    @Test
+    public void setGroup_editedGroupIsSameGroup_success() {
+        Group group = new Group(new Name("Soc Friends"));
+        uniqueGroupList.addGroup(group);
+        uniqueGroupList.setGroup(group, group);
+        UniqueGroupList expectedUniqueGroupList = new UniqueGroupList();
+        expectedUniqueGroupList.addGroup(group);
+        assertEquals(expectedUniqueGroupList, uniqueGroupList);
     }
 
     @Test
