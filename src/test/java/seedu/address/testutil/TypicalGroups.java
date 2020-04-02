a2b85b68886a508eaacb2bd10662aa8fe425d999
Raivat Shah <hiraivat@gmail.com>
2020-04-02 23:19:59 +0800

    Add Typical Groups

--- src/test/java/seedu/address/testutil/TypicalGroups.java
+++ src/test/java/seedu/address/testutil/TypicalGroups.java
@@ -1,12 +1,35 @@
 package seedu.address.testutil;
 
-import seedu.address.model.group.Group;
-import seedu.address.model.person.Name;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import seedu.address.model.AddressBook;
+import seedu.address.model.group.Group;
 /**
  * A utility class containing list of {@code Group} objects to be used in tests.
  */
 public class TypicalGroups {
 
-    public static final Group SOC = new Group(new Name("SoC Friends"));
+    public static final Group SOC = new GroupBuilder().withName("SoC Friends").withId(1).withTime("1h 1m").build();
+    public static final Group RC = new GroupBuilder().withName("RC Friends").withId(2).withTime("2h 2m").build();
+    public static final Group NS = new GroupBuilder().withName("NS Friends").withId(3).withTime("0h 30m").build();
+
+    private TypicalGroups() {
+    }
+
+    /**
+     * Returns an {@code AddressBook} with all the typical groups
+     */
+    public static AddressBook getTypicalAddressBook() {
+        AddressBook ab = new AddressBook();
+        for (Group group : getTypicalGroups()) {
+            ab.addGroup(group);
+        }
+        return ab;
+    }
+
+    public static List<Group> getTypicalGroups() {
+        return new ArrayList<>(Arrays.asList(SOC, RC, NS));
+    }
 }
