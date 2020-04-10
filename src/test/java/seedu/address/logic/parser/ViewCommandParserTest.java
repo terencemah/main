0ee1dce809e51c2a0a2ea956514372c87588caf5
l2ejected <ernestlian@hotmail.com>
2020-04-10 20:43:26 +0800

    test cases added

--- src/test/java/seedu/address/logic/parser/ViewCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ViewCommandParserTest.java
@@ -1,9 +1,16 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.ViewCommand;
+import seedu.address.logic.commands.ViewCommand_group;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 public class ViewCommandParserTest {
@@ -24,5 +31,26 @@ public class ViewCommandParserTest {
     public void parse_invalidParameter_throwsParseException() {
         assertThrows(ParseException.class, () -> vcp.parse("1"));
         assertThrows(ParseException.class, () -> vcp.parse("1 t"));
+    }
+
+    @Test
+    public void parse_emptyIndex_failure() {
+        String userInput = " places";
+        assertParseFailure(vcp, userInput, String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT,
+                ViewCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_emptyParameter_failure() {
+        String userInput = "1";
+        assertParseFailure(vcp, userInput, ViewCommand.MESSAGE_INVALID_PARAMETER + " " +
+                ViewCommand.MESSAGE_USAGE);
+    }
+
+    @Test
+    public void parse_invalidParameter_failure() {
+        String userInput = "1 test";
+        assertParseFailure(vcp, userInput, ViewCommand.MESSAGE_INVALID_PARAMETER
+                + ViewCommand.MESSAGE_USAGE);
     }
 }
