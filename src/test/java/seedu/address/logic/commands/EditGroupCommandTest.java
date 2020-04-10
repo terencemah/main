4d5bf3833168ee5f85d1d4ea6dedd9abad949a2d
Raivat Shah <hiraivat@gmail.com>
2020-04-10 12:37:01 +0800

    add EditGroupDescriptorBuilder

--- src/test/java/seedu/address/logic/commands/EditGroupCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditGroupCommandTest.java
@@ -1,4 +1,26 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.testutil.TypicalGroups.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.EditGroupCommand.EditGroupDescriptor;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.group.Group;
+import seedu.address.testutil.GroupBuilder;
+
 public class EditGroupCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_allFieldsSpecifiedUnfilteredList_success() {
+        Group firstGroup = model.getFilteredGroupList().get(0);
+        Group editedGroup = new GroupBuilder().build();
+
+        EditGroupDescriptor descriptor = new EditGroupDescriptor(editedGroup).bu
+
+    }
 }
---
+++ src/test/java/seedu/address/testutil/EditGroupDescriptorBuilder.java
@@ -0,0 +1,55 @@
+package seedu.address.testutil;
+
+import java.util.ArrayList;
+
+import seedu.address.logic.commands.EditGroupCommand.EditGroupDescriptor;
+import seedu.address.model.group.Group;
+import seedu.address.model.person.Name;
+
+/**
+ * A utility class to build {@code EditGroupDescriptor} objects.
+ */
+public class EditGroupDescriptorBuilder {
+
+    private EditGroupDescriptor descriptor;
+
+    public EditGroupDescriptorBuilder(EditGroupDescriptor descriptor) {
+        this.descriptor = new EditGroupDescriptor(descriptor);
+    }
+
+    /**
+     * Creates a {@code EditGroupDescriptor} based on {@code Group} instance provided.
+     * @param group
+     */
+    public EditGroupDescriptorBuilder(Group group) {
+        this.descriptor = new EditGroupDescriptor();
+        this.descriptor.setName(group.getName());
+        this.descriptor.setMemberIds(group.getMembers());
+    }
+
+    /**
+     * Sets the {@code Name} of the {@code EditGroupDescriptor}
+     */
+    public EditGroupDescriptorBuilder withName(String name) {
+        descriptor.setName(new Name(name));
+        return this;
+    }
+
+    /**
+     * Sets the member ids of the {@code EditGroupDescriptor}
+     */
+    public EditGroupDescriptorBuilder withMemberIds(ArrayList<Integer> ids) {
+        descriptor.setMemberIds(ids);
+        return this;
+    }
+
+    /**
+     * Builds an edit group descriptor based on the descriptor in
+     * the current {@code EditGroupDescriptorBuilder } instance.
+      * @return
+     */
+    public EditGroupDescriptor build() {
+        return descriptor;
+    }
+
+}
