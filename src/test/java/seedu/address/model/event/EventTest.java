bb1e5d196cbdb9b31249c1e8bc5cbe9b8a6d8ba0
l2ejected <ernestlian@hotmail.com>
2020-04-12 17:59:24 +0800

    test cases added for Event class

---
+++ src/test/java/seedu/address/model/event/EventTest.java
@@ -0,0 +1,43 @@
+package seedu.address.model.event;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.person.Time;
+
+public class EventTest {
+
+    Event firstEvent = new Event("something", "somewhere", new Time(11, 11));
+    Event secondEvent = new Event("something", "somewhere", new Time(11, 11));
+
+    @Test
+    public void equals() {
+
+        //different EventId -> return false
+        assertFalse(firstEvent.equals(secondEvent));
+
+        //same Event -> return true
+        assertTrue(firstEvent.equals(firstEvent));
+        assertTrue(secondEvent.equals(secondEvent));
+    }
+
+    @Test
+    public void isSameEvent() {
+        assertTrue(firstEvent.isSameEvent(firstEvent));
+        assertFalse(firstEvent.isSameEvent(secondEvent));
+    }
+
+    @Test
+    public void sameTime() {
+        Event thirdEvent = new Event("something", "somewhere", new Time(22,22));
+        thirdEvent.setTime(new Time(11,11));
+        assertTrue(thirdEvent.getTime().equals(new Time(11, 11)));
+    }
+
+    @Test
+    public void toString_success() {
+        firstEvent.toString().equals("Event: something place: somewhere for 11h 11m");
+    }
+}
