fe27a7020c911243237f2a3abf3a9ee47c9ce466
Raivat Shah <hiraivat@gmail.com>
2020-04-12 13:06:07 +0800

    add testcase for optional field  in AddGroupCommandParserTest

--- src/test/java/seedu/address/logic/parser/AddGroupCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddGroupCommandParserTest.java
@@ -3,9 +3,11 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.commands.CommandTestUtil.MEMBER_DESC_1;
 import static seedu.address.logic.commands.CommandTestUtil.MEMBER_DESC_2;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_RC;
+import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_SOC;
 import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalGroups.RC_NO_TIME;
+import static seedu.address.testutil.TypicalGroups.SOC_NO_TIME;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,9 +23,22 @@ public class AddGroupCommandParserTest {
     public void parse_allFields_success() {
         Group expectedGroup = new GroupBuilder(RC_NO_TIME).build();
 
+        // normal execution
         assertParseSuccess(parser,
                 PREAMBLE_WHITESPACE + NAME_DESC_RC + MEMBER_DESC_1 + MEMBER_DESC_2,
                 new AddGroupCommand(expectedGroup)
         );
+
+        // multiple names - last name accepted
+        assertParseSuccess(parser,
+                NAME_DESC_SOC + NAME_DESC_RC + MEMBER_DESC_1 + MEMBER_DESC_2,
+                new AddGroupCommand(expectedGroup)
+        );
+    }
+
+    @Test
+    public void parse_withoutMembersIds_success() {
+        Group expectedGroup = new GroupBuilder(SOC_NO_TIME).build();
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_SOC, new AddGroupCommand(expectedGroup));
     }
 }
